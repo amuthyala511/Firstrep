1c00a389563cf22be9a3497be7dc7a1c0d1fcee3
Aishwarya <amuthyala511@gmail.com>
2020-07-24 12:50:41 +0530

    going to fight conflict

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,7 @@ public class Helloworld
 {
 	public static void main(String args[])
 	{
-		System.out.println("HelloWorld");
-		System.out.println("hi,this code is modified");
+		System.out.println("HellotheWorld");
+		System.out.println("hi,this bad code is modified by me");
 	}
 }
