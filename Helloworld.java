193ec2e155b77b1fca50d8d26085b66eb525a76e
Aishwarya <amuthyala511@gmail.com>
2020-10-22 16:24:07 +0530

    added files

--- Helloworld.java
+++ Helloworld.java
@@ -1,9 +1,13 @@
 public class Helloworld
 {
+	public static void add()
+	{
+		System.out.println("Inside add method");
+	}
 	public static void main(String args[])
 	{
 		System.out.println("This is a my working application");
 		System.out.println("This is my new feature implemented in develop branch");
-		System.out.println("This is my another feature implemented in feature branch");
+		add();
 	}
 }
